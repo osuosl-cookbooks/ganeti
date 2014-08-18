08d54f78723c9a94704d08685f0eabd6733ad884
Lance Albertson <lance@osuosl.org>
2014-08-18 10:39:15 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'ganeti'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/Configures ganeti'
+long_description 'Installs/Configures ganeti'
+version          '0.1.0'
+
