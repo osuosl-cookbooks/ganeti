693af5f5be70d4bd3af427a9e92dc763acedadc3
Jordan Evans <jordane@osuosl.org>
2014-08-19 10:05:06 -0700

    add rapi_users function

---
+++ libraries/rapi_users.rb
@@ -0,0 +1,16 @@
+require 'digest'
+
+# rapi_users expects h to be a Hash that looks like:
+# h['username'] = {
+#   'password' => 'secret',
+#   'write' => true # or false
+def rapi_users(h)
+  rapi_users = []
+  h.each_pair do |username,opts|
+    md5 =  username + ":Ganeti Remote API:" + opts['password']
+    md5 = Digest::MD5.new().update(md5).hexdigest
+    rapi_users << "#{username} {HA1}#{md5} #{opts['write'] ? "write" : ""}"
+  end
+  return rapi_users.join("\n")
+end
+    
