5b5882e4e622e0fc3264d159688b15af659c0ea0
Jordan Evans <jordane@osuosl.org>
2014-08-21 16:36:58 -0700

    don't loop over id key of data bags

--- libraries/rapi_users.rb
+++ libraries/rapi_users.rb
@@ -6,7 +6,7 @@ require 'digest'
 #   'write' => true # or false
 def rapi_users(h)
   rapi_users = []
-  h.each_pair do |username,opts|
+  h.reject {|k,v| k == 'id' }.each_pair do |username,opts|
     md5 =  username + ":Ganeti Remote API:" + opts['password']
     md5 = Digest::MD5.new().update(md5).hexdigest
     rapi_users << "#{username} {HA1}#{md5} #{opts['write'] ? "write" : ""}"
