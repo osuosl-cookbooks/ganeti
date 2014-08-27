6c6a8404e605e8b935e440b89bb7f4057bf5daff
Lance Albertson <lance@osuosl.org>
2014-08-27 09:29:04 -0700

    Reorder a few attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,23 +3,23 @@ default['ganeti']['hypervisor'] = "kvm"
 default['ganeti']['master-node'] = nil
 default['ganeti']['bin-path'] = "/usr/sbin"
 default['ganeti']['data_bag']['rapi_users'] = "rapi_users"
+default['modules']['modules']= [
+  'drbd minor_count=128 usermode_helper=/bin/true',
+  'kvm' ]
 # Cluster initialization attributes
-default['ganeti']['cluster']['extra-opts'] = nil
+default['ganeti']['cluster']['name'] = nil
 default['ganeti']['cluster']['disk-templates'] = [ 'plain', 'drbd' ]
 default['ganeti']['cluster']['master-netdev'] = "br0"
 default['ganeti']['cluster']['enabled-hypervisors'] = [ 'kvm' ]
 default['ganeti']['cluster']['nic']['mode'] = "bridged"
 default['ganeti']['cluster']['nic']['link'] = "br0"
-default['ganeti']['cluster']['name'] = nil
+default['ganeti']['cluster']['extra-opts'] = nil
 
 # YUM repo for Ganeti
 default['ganeti']['yum']['uri'] = "http://jfut.integ.jp/linux/ganeti/$releasever/$basearch"
 default['ganeti']['yum']['description'] = "Integ Ganeti Packages $releasever - $basearch"
 default['ganeti']['yum']['gpgcheck'] = false
 default['ganeti']['yum']['gpgkey'] = nil
-default['modules']['modules']= [
-  'drbd minor_count=128 usermode_helper=/bin/true',
-  'kvm' ]
 
 default['ganeti']['package_name'] = value_for_platform_family(
   {
