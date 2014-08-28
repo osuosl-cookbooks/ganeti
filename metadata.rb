c05b428e19bf270747293146f45e08f0af806f1c
Lance Albertson <lance@osuosl.org>
2014-08-28 11:41:52 -0700

    Support ubuntu/centos in metadata

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures ganeti'
 long_description 'Installs/Configures ganeti'
 version          '0.1.0'
+supports         'ubuntu', '~> 12.04'
+supports         'centos', '~> 6.0'
 
 depends          'apt'
 depends          'lvm'
