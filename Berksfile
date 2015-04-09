0db423561d10f1437f6af2637943acfc174d62c0
Lance Albertson <lance@osuosl.org>
2015-04-09 09:54:15 -0700

    Rubocop fixes

--- Berksfile
+++ Berksfile
@@ -1,10 +1,10 @@
-source "https://api.berkshelf.com"
+source 'https://api.berkshelf.com'
 
-cookbook "apt"
-cookbook "hostsfile"
-cookbook "lvm"
-cookbook "modules", git: "git@github.com:osuosl-cookbooks/modules-cookbook"
-cookbook "yum-elrepo"
-cookbook "yum-epel"
+cookbook 'apt'
+cookbook 'hostsfile'
+cookbook 'lvm'
+cookbook 'modules', git: 'git@github.com:osuosl-cookbooks/modules-cookbook'
+cookbook 'yum-elrepo'
+cookbook 'yum-epel'
 
 metadata
--- recipes/_test.rb
+++ recipes/_test.rb
@@ -18,15 +18,15 @@
 #
 # XXX: This recipe is only used for testing and should not be used otherwise.
 # Setup a fake ganeti cluster ip that's resolvable
-hostsfile_entry "192.168.125.10" do
-  hostname "ganeti.local"
+hostsfile_entry '192.168.125.10' do
+  hostname 'ganeti.local'
   action :create
 end
 # Make sure the hostname isn't set to localhost
 # NOTE: This usually happens on vagrant systems
 hostsfile_entry node['network']['interfaces']['eth0']['addresses'].keys[1] do
   hostname node['fqdn']
-  aliases [ node['hostname'] ]
+  aliases [node['hostname']]
   unique true
   action :create
 end
