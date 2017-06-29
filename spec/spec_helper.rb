e3703795ef53bf3bbfe7a27adb23e2e6da0e72db
Lance Albertson <lance@osuosl.org>
2017-06-29 16:43:15 -0700

    Update per our code_generator cookbook

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'chefspec'
+require 'chefspec/berkshelf'
+
+ChefSpec::Coverage.start! { add_filter 'ganeti' }
+
+
+CENTOS_6 = {
+  platform: 'centos',
+  version: '6.7'
+}.freeze
+
+UBUNTU_12_04 = {
+  platform: 'ubuntu',
+  version: '12.04'
+}.freeze
+
+ALL_PLATFORMS = [
+  CENTOS_6,
+  UBUNTU_12_04
+].freeze
+
+RSpec.configure do |config|
+  config.log_level = :fatal
+end
