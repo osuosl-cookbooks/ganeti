2c340607cdc09cfe57316cc123645b0d516f06ae
Lance Albertson <lance@osuosl.org>
2017-06-29 16:58:05 -0700

    Foodcritic fixes

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name             'ganeti'
 maintainer       'Oregon State University'
 maintainer_email 'chef@osuosl.org'
+source_url       'https://github.com/osuosl-cookbooks/ganeti'
+issues_url       'https://github.com/osuosl-cookbooks/ganeti/issues'
 license          'Apache 2.0'
 description      'Installs/Configures ganeti'
 long_description 'Installs/Configures ganeti'
