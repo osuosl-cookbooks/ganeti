e028fcf6406ab3b2e92c028f24dcc34c94c2da21
Kerry Vance <vancelot@osuosl.org>
2019-08-22 13:18:39 -0700

    fix

--- test/integration/default/serverspec/server_spec.rb
+++ test/integration/default/serverspec/server_spec.rb
@@ -1,8 +1,26 @@
+require 'serverspec'
+
+set :backend, :exec
+
 case os[:family].downcase
 when 'redhat', 'centos'
-  packages = %w( ganeti lvm2 )
+  case os[:release].to_i
+  when 6
+    packages = %w(
+      ganeti
+      lvm2
+    )
+  when 7
+    packages = %w(
+      ganeti
+      lvm2
+    )
+  end
 when 'debian', 'ubuntu'
-  packages = %w( ganeti2 lvm2 )
+  packages = %w(
+    ganeti2
+    lvm2
+  )
 end
 
 packages.each do |p|
@@ -17,7 +35,7 @@ end
 
 # Make sure lvm.conf excludes drbd
 describe file('/etc/lvm/lvm.conf') do
-  its('content') do
+  its(:content) do
     should match(%r{filter = \[ "a/.\*/", "r\|/dev/drbd.\*\|" \]})
   end
 end
@@ -29,16 +47,16 @@ end
 
 describe file('/root/.ssh') do
   it { should be_directory }
-  its('owner') { should eq 'root' }
-  its('group') { should eq 'root' }
-  its('mode') { should cmp '0700' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode 700 }
 end
 
 describe file('/var/lib/ganeti/rapi') do
   it { should be_directory }
-  its('owner') { should eq 'root' }
-  its('group') { should eq 'root' }
-  its('mode') { should cmp '0750' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode 750 }
 end
 
 describe file('/etc/cron.d/ganeti') do
