348170a2835b5bc7fbfe7e41730068f442f4dd83
Kerry Vance <vancelot@osuosl.org>
2019-09-25 15:04:43 -0700

    ChefSpec fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,14 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-ChefSpec::Coverage.start! { add_filter 'ganeti' }
-
 CENTOS_6 = {
   platform: 'centos',
-  version: '6.9',
+  version: '6',
 }.freeze
 
 CENTOS_7 = {
   platform: 'centos',
-  version: '7.4.1708',
+  version: '7',
 }.freeze
 
 ALL_PLATFORMS = [
@@ -25,5 +23,5 @@ shared_context 'common_stubs' do
 end
 
 RSpec.configure do |config|
-  config.log_level = :fatal
+  config.log_level = :warn
 end
