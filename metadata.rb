359ce5a0b4e3a505824d75c3f698798743bdd52b
Manatee <github@osuosl.org>
2020-08-25 22:45:30 +0000

    Automatic minor-level version bump to v3.1.0 by Jenkins

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url       'https://github.com/osuosl-cookbooks/ganeti/issues'
 license          'Apache-2.0'
 chef_version     '>= 14.0'
 description      'Installs/Configures ganeti'
-version          '3.0.0'
+version          '3.1.0'
 
 supports         'centos', '~> 7.0'
 
